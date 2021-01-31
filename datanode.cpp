865d7fe8a9ef603c9df977cc54aa8ce367496000
starmlerp <filip.jamuljak@gmail.com>
2021-01-31 02:26:17 +0000

    started working on the class function code

--- datanode.cpp
+++ datanode.cpp
@@ -1,15 +1,25 @@
 #include "datanode.h"
 
-datanode::datanode(){
-	data = 0;
-	parent = (datanode*)_parent;
+datanode::datanode(size_t _size){
+	size = _size;
+	child = new datanode[size];
 }
 datanode::~datanode(){
 	delete child;
 }
 size_t datanode::size(){
-	return data;
+	return size;
 }
 datanode* datanode::append(){
-	return 0;
+	size++;
+	datanode* holder = new datanode[size];
+	delete child;
+	child = holder;
+	return (datanode*)(child+size-1);
+}
+datanode* datanode::getElement(size_t num){
+	if(num < size){
+		return (datanode*)(child + num);
+	}
+	return nullptr;
 }
--- datanode.h
+++ datanode.h
@@ -5,21 +5,15 @@
 namespace adn{
 	class node{
 		private:
+		size_t size=0;
 		datanode* child=nullptr; //store the children instances
 		public:
 		std::string line;//the component defining the node,
-		datanode();//when initialized, a pointer to parent node must be passed
+		datanode(size_t _size);
 		~datanode();
-
 		size_t size(); //get the number of children to this node
-		datanode* append();//append elements to the end of the node
+		datanode* append();//since the children of the datanode are stored as contiguous array, it is reccomended to avoid usage of this function, since its memory and processor expensive
 		datanode* getElement(size_t num);
-		void remove(datanode* target);
-		parser parseNode();
-	};
-	class parser{
-		std::string specifier;
-		std::string complement;
 	};
 }
 #endif
