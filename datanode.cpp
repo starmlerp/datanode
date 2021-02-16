deff805612c80e17c4f5c59e7e9783a61e198c80
starmlerp <filip.jamuljak@gmail.com>
2021-02-16 19:15:34 +0100

    note to self: commit more often

--- datanode.cpp
+++ datanode.cpp
@@ -1,24 +1,24 @@
 #include "datanode.h"
 
-datanode::datanode(size_t _size){
-	size = _size;
-	child = new datanode[size];
+datanode::datanode(size_t _nsize){
+	nsize = _nsize;
+	child = new datanode[nsize];
 }
 datanode::~datanode(){
 	delete child;
 }
 size_t datanode::size(){
-	return size;
+	return nsize;
 }
 datanode* datanode::append(){
-	size++;
-	datanode* holder = new datanode[size];
+	nsize++;
+	datanode* holder = new datanode[nsize];
 	delete child;
 	child = holder;
-	return (datanode*)(child+size-1);
+	return (datanode*)(child+nsize-1);
 }
 datanode* datanode::getElement(size_t num){
-	if(num < size){
+	if(num < nsize){
 		return (datanode*)(child + num);
 	}
 	return nullptr;
