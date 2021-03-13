728c38beababd04f978f8eb631105d391359c8f4
starmlerp <filip.jamuljak@gmail.com>
2021-03-13 10:55:32 +0100

    small fixes

--- datanode.cpp
+++ datanode.cpp
@@ -1,25 +1,27 @@
 #include "datanode.h"
-
-datanode::datanode(size_t _nsize){
-	nsize = _nsize;
-	child = new datanode[nsize];
-}
-datanode::~datanode(){
-	delete child;
-}
-size_t datanode::size(){
-	return nsize;
-}
-datanode* datanode::append(){
-	nsize++;
-	datanode* holder = new datanode[nsize];
-	delete child;
-	child = holder;
-	return (datanode*)(child+nsize-1);
-}
-datanode* datanode::getElement(size_t num){
-	if(num < nsize){
-		return (datanode*)(child + num);
+namespace adn{
+	datanode::datanode(size_t _nsize)
+	{
+		nsize = _nsize;
+	        if(nsize)child = new datanode[nsize];
 	}
-	return nullptr;
+	datanode::~datanode(){
+		delete child;
+	}
+	size_t datanode::size(){
+		return nsize;
+	}
+	datanode* datanode::append(){
+		nsize++;
+		datanode* holder = new datanode[nsize];
+		delete child;
+		child = holder;
+		return (datanode*)(child+nsize-1);
+	}
+	datanode* datanode::getElement(size_t num){
+		if(num < nsize){
+			return (datanode*)(child + num);
+		}
+		return nullptr;
+	}
 }
--- datanode.h
+++ datanode.h
@@ -9,7 +9,7 @@ namespace adn{
 		datanode* child=nullptr; //store the children instances
 		public:
 		std::string line;//the component defining the node,
-		datanode(size_t _nsize);
+		datanode(size_t _nsize = 0);
 		~datanode();
 		size_t size(); //get the number of children to this node
 		datanode* append();//since the children of the datanode are stored as contiguous array, it is reccomended to avoid usage of this function, since its memory and processor expensive
